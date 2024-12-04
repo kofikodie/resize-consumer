ca1b9ed36632fe9b74cadae320298d3ef231de11
Kofi Kodie <k.kodieaddo@gmail.com>
2024-12-04 16:35:42 +0100

    remove comments

--- src/stream-helpler.ts
+++ src/stream-helpler.ts
@@ -1,11 +1,6 @@
 import { Readable } from "node:stream";
 
 export class StreamHelper {
-    /**
-     * Converts a Readable stream into a Buffer.
-     * @param stream - The Readable stream to convert.
-     * @returns A Promise resolving to a Buffer.
-     */
     static async streamToBuffer(stream: Readable): Promise<Buffer> {
         return new Promise((resolve, reject) => {
             const chunks: Uint8Array[] = [];
